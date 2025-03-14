cfc1011dc70446237adcc8f9a0600bd6fea13d1a
Felix Engelmann <fe-github@nlogn.org>
2025-03-14 15:19:12 -0400

    csrf

--- dockedit/settings.py
+++ dockedit/settings.py
@@ -26,6 +26,9 @@ DEBUG = bool(os.environ.get("DEBUG", default=False))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    "DJANGO_TRUSTED_ORIGINS", "http://localhost:8000"
+).split(",")
 
 INTERNAL_IPS = [
     # ...
